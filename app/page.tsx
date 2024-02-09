aff1dcba37632ff9940cd2c282b3b98f039bda09
HASSAN <hassan.aburesha@gmail.com>
2024-02-09 11:05:46 +0100

    Add HomepageTitle component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import HomepageTitle from "./components/HomepageTitle";
 import MovieList from "./components/MovieList";
 
 export default function Home() {
-    return <MovieList />;
+    return (
+        <>
+            <HomepageTitle />
+            <MovieList />
+        </>
+    );
 }
