e39b489b3efdac782849db0727caf69f90577b01
HASSAN <hassan.aburesha@gmail.com>
2024-02-14 10:50:23 +0100

    Refactor MovieCardSkeleton component to display multiple skeletons

--- app/components/MovieCardSkeleton.tsx
+++ app/components/MovieCardSkeleton.tsx
@@ -3,25 +3,37 @@ import Skeleton from "@mui/material/Skeleton";
 
 const MovieCardSkeleton = () => {
     return (
-        <div style={{ marginTop: 50 }}>
-            <Box
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    width: "210px",
-                }}
-            >
-                <Skeleton
-                    sx={{ bgcolor: "grey.900" }}
-                    variant="rectangular"
-                    width={210}
-                    height={118}
-                />
-                <Box sx={{ pt: 0.5 }}>
-                    <Skeleton sx={{ bgcolor: "grey.900" }} />
-                    <Skeleton sx={{ bgcolor: "grey.900" }} />
-                </Box>
-            </Box>
+        <div
+            style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-around",
+                padding: "16px",
+                margin: "16px",
+            }}
+        >
+            {Array(4).fill(
+                <div style={{ marginTop: 50 }}>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            width: "210px",
+                        }}
+                    >
+                        <Skeleton
+                            sx={{ bgcolor: "grey.900" }}
+                            variant="rectangular"
+                            width={210}
+                            height={118}
+                        />
+                        <Box sx={{ pt: 0.5 }}>
+                            <Skeleton sx={{ bgcolor: "grey.900" }} />
+                            <Skeleton sx={{ bgcolor: "grey.900" }} />
+                        </Box>
+                    </Box>
+                </div>
+            )}
         </div>
     );
 };
