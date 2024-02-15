bf5216160e9d725da31946aab7caf76dfb3bb03e
HASSAN <hassan.aburesha@gmail.com>
2024-02-15 22:38:28 +0100

    Update homepage title to reflect various platforms

--- app/components/HomepageTitle.tsx
+++ app/components/HomepageTitle.tsx
@@ -5,7 +5,7 @@ function HomepageTitle() {
     return (
         <Box className="page-title">
             <Typography variant="h4" component="h1">
-                Now Playing in Cinemas ...
+                Now playing on various platforms ...
             </Typography>
         </Box>
     );
