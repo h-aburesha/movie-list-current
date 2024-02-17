c4fd29357a707c17f90a9a81e31d19f4aadac3d1
HASSAN <hassan.aburesha@gmail.com>
2024-02-17 11:37:33 +0100

    Add framer-motion animation to HomepageTitle component

--- app/components/HomepageTitle.tsx
+++ app/components/HomepageTitle.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 // ** MUI Imports
 import { Box, Typography } from "@mui/material";
 
+// ** Third Party Imports
+import { motion } from "framer-motion";
+
 function HomepageTitle() {
     return (
-        <Box className="page-title">
-            <Typography variant="h4" component="h1">
-                Now playing on various platforms ...
-            </Typography>
-        </Box>
+        <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.8, duration: 1.2 }}
+        >
+            <Box className="page-title">
+                <Typography variant="h4" component="h1">
+                    Now playing on various platforms ..
+                </Typography>
+            </Box>
+        </motion.div>
     );
 }
 
